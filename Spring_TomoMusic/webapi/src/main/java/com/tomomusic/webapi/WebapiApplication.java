7c97536671d3c0acaeb0403a0e5c6c1e1f695022
도영 <55948585+ohdoyoung@users.noreply.github.com>
2025-01-25 16:37:18 +0900

    스프링 폴더 생성

---
+++ Spring_TomoMusic/webapi/src/main/java/com/tomomusic/webapi/WebapiApplication.java
@@ -0,0 +1,13 @@
+package com.tomomusic.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapiApplication.class, args);
+	}
+
+}
