8796c59c259450e94d47dc9f909bcc080c40fb56
doyoung <dhehdud456@naver.com>
2025-02-10 16:27:48 +0900

    02.10 작업( 다이어리 텍스트 구현)

---
+++ SoundScape/SoundScape/DiaryView.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryView.swift
+//  SoundScape
+//
+//  Created by 오도영 on 2/10/25.
+//
+
+import SwiftUI
+
+struct DiaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiaryView()
+}
